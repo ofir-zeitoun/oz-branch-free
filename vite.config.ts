edf955564f4aabba59847d7a44891449268017b6
Ofir <ofir.zeitoun@gmail.com>
2023-10-13 11:26:57 +0300

    exclude spec from lib

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,13 @@ import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [dts({ include: ["src"], insertTypesEntry: true })],
+  plugins: [
+    dts({
+      include: ["src"],
+      insertTypesEntry: true,
+      exclude: ["src/**/*.spec.*"],
+    }),
+  ],
   build: {
     lib: {
       entry: "src/index.ts",
